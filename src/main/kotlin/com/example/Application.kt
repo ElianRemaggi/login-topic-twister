092018677065e11c5a4ad5ec5dad4f6bdec38ec7
Elian <elianremaggi@gmail.com>
2022-12-03 21:50:32 -0300

    Revert "add"
    This reverts commit 26ce0dd1d4d39bd310b998958cd83bf3e58a907d.

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,7 +6,7 @@ import com.example.plugins.*
 
 fun main() {
     //embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-    embeddedServer(Netty, port = System.getenv($PORT) {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
